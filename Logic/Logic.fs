83f263dca1c05effeb0ebd3f3185b54d65a22ec6
Nico Linder <nicolinder@icloud.com>
2016-11-25 12:15:19 +0100

    implemented customisable questions via textfile „quiz.txt“. Closes #2

--- Logic/Logic.fs
+++ Logic/Logic.fs
@@ -4,6 +4,14 @@ type QuestionString = Question of string
 type AnswerString = string
 type AnswerIndex = | A | B | C | D
 
+let indexFromString string =
+    match string with
+    | "A" | "a" | "1" -> A
+    | "B" | "b" | "2" -> B
+    | "C" | "c" | "3" -> C
+    | "D" | "d" | "4" -> D
+    | _ -> failwith "no valid string from AnswerIndex"
+
 type Question = {question: QuestionString; answers: AnswerString * AnswerString * AnswerString * AnswerString; correct: AnswerIndex}
 type public Quiz = {questions: Question list; results: bool list; nrOfQuestions: int}
 
@@ -14,11 +22,11 @@ let private addQuestion quiz question =
     match quiz with
     | {questions = questions; results = results; nrOfQuestions = length} -> {questions = questions @ [question]; results = results; nrOfQuestions = length + 1}
 
-let initQuiz =
+(*let initQuiz =
     let quiz = emptyQuiz
     let quiz' = addQuestion quiz (question "Testfrage! Eine sehr sehr lange Testfrage, um die Zeilenumbrüche etc. zu testen! BOAH wie geil!" A "Answer A" "Answer B" "Answer C" "Answer D")
     let quiz'' = addQuestion quiz' (question "Dies ist die zweite Frage! Unglaublich..." B "Answer A2" "Answer B2" "Answer C2" "Answer R2D2")
-    quiz''
+    quiz''*)
 
 // Simple Getter
 let private getQuestion quiz =
@@ -65,12 +73,41 @@ let chooseAnswer quiz answer =
         failwith "Quiz has no valid state. No question to choose an answer from!"
     | {questions = _::restQuestions; results = results; nrOfQuestions = length} ->
         match checkAnswer quiz answer with
-        | true ->
-            let quiz' = {questions = restQuestions; results = results @ [true]; nrOfQuestions = length}
-            quiz'
-        | false ->
-            let quiz' = {questions = restQuestions; results = results @ [false]; nrOfQuestions = length}
-            quiz'
+        | true -> {questions = restQuestions; results = results @ [true]; nrOfQuestions = length}
+        | false -> {questions = restQuestions; results = results @ [false]; nrOfQuestions = length}
+
+// Questions from file
+let readLines filePath = 
+    let lines = System.IO.File.ReadLines(filePath)
+    List.ofSeq lines
+
+let getArgsFromLine (line : string) =
+    let args = line.Split ':'
+    List.ofArray args
+
+let addQuestionFromLine (line : string) quiz =
+    let args = getArgsFromLine line
+    match args with
+    | questionString::correct::answerA::answerB::answerC::[answerD] ->
+        let index = indexFromString correct
+        addQuestion quiz (question questionString index answerA answerB answerC answerD)
+    | _ -> failwith "line has wrong number of arguments"
+
+let rec addQuestionsFromLines lines quiz =
+    match lines with
+    | line::rest ->
+        let quiz' = addQuestionFromLine line quiz
+        addQuestionsFromLines rest quiz'
+    | [] ->
+        quiz
+
+let addQuestionsFromFile filePath quiz =
+    let lines = readLines filePath
+    addQuestionsFromLines lines quiz
+    
+let initQuizFromFile filePath =
+    let quiz = emptyQuiz
+    addQuestionsFromFile filePath quiz
 
 // Define object functions for use in C#
 type Quiz with
