c6f0ee205fb6e1c9efd95772784b6aadf2cfcd41
Nico Linder <nicolinder@icloud.com>
2016-11-27 09:47:35 +0100

    include changelog file (empty for now) change AssemblyVersion to 0.0.0.0 for now

--- Logic/AssemblyInfo.fs
+++ Logic/AssemblyInfo.fs
@@ -2,17 +2,17 @@
 open System.Reflection
 open System.Runtime.CompilerServices
 
-[<assembly: AssemblyTitle("Logic")>]
+[<assembly: AssemblyTitle("QuizPresentator Logic")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("")>]
-[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyCopyright("2016 Nico Linder")>]
 [<assembly: AssemblyTrademark("")>]
 
 // The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
 
-[<assembly: AssemblyVersion("0.1.*")>]
+[<assembly: AssemblyVersion("0.0.0.0")>]
 
 //[<assembly: AssemblyDelaySign(false)>]
 //[<assembly: AssemblyKeyFile("")>]
